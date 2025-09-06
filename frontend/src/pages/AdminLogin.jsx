740ddc1301eb21b087f7d766b66db6013dfdb9ee
amritaa01Maurya <mauryaamrita835@gmail.com>
2025-09-06 09:21:56 +0530

    add login pages

---
+++ frontend/src/pages/AdminLogin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginCard from "../components/LoginCard";
+
+const AdminLogin = () => {
+  return <LoginCard role="Admin" demoEmail="admin@college.edu" />;
+};
+
+export default AdminLogin;
