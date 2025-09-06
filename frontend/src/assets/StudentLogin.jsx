bbc7ef2fbafe969465f7154514909d7f4c56accd
amritaa01Maurya <mauryaamrita835@gmail.com>
2025-09-06 10:46:09 +0530

    add login pages

---
+++ frontend/src/assets/StudentLogin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginCard from "../components/LoginCard";
+
+const StudentLogin = () => {
+  return <LoginCard role="Student" demoEmail="john.student@college.edu" />;
+};
+
+export default StudentLogin;
---
+++ frontend/src/pages/TeacherLogin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginCard from "../components/LoginCard";
+
+const TeacherLogin = () => {
+  return <LoginCard role="Teacher" demoEmail="jane.teacher@college.edu" />;
+};
+
+export default TeacherLogin;
